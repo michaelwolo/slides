f1836a01e1216748f4860f067e0ca6c8a0806f59
Michael Woloszuk <mwoloszuk@gmail.com>
2016-03-01 08:18:20 -0600

    Simplify state

--- app/index.js
+++ app/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { List, Map } from 'immutable';
+import { state } from './store/base';
 import initialization from './store/initialization';
 import Application from './containers/application';
-
-const state = {
-  slides: List(['Slide Un', 'Slide Deux', 'Slide Trois', 'Slide Quatre']),
-  indexes: Map({
-    current: 0,
-    previous: null
-  })
-}
 
 const store = initialization(state);
 
