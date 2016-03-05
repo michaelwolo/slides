cedffe7cad8618fe25ea38234d0d2153426416da
Michael Woloszuk <mwoloszuk@gmail.com>
2016-03-05 17:36:15 -0600

    Remove import for unnecessary dependency

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { state } from './store/base';
 import initialization from './store/initialization';
 import Application from './containers/application';
-import Prism from 'prismjs';
 
 const store = initialization(state);
 
